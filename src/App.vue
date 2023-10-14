b70c4d026e11806eb51cc124b6ee5a679cab0f76
Alex Matsuev <alex.matsuev@gmail.com>
2023-10-14 16:24:28 +0700

    Update

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+   <h2>ToDo Application</h2>
+   <ToDoList :list="itemList" @delete="onDelete" />
+   <AppendForm @append="onAppend" />
+</template>
+
+<script setup>
+import AppendForm from '../components/AppendForm.vue';
+import ToDoList from '../components/ToDoList.vue';
+import { reactive } from 'vue';
+
+const itemList = reactive([
+   "gdhsjfksajdhgf adsg",
+   "gdsahjkgfahjdsgfkjah",
+   "gdsghsjdgf asgdfhjkagsdfk"
+])
+
+function onDelete(index)
+{
+   itemList.splice(index, 1)
+}
+
+function onAppend(newItem)
+{
+   itemList.push(newItem)
+}
+
+</script>
